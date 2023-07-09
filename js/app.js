a1ef2110d0d43b280e97fb174ffef70c12517f8c
maltsevalex <57605252+MasterSancho@users.noreply.github.com>
2023-07-09 12:30:38 +0300

    008-->Set Calorie Limit

--- js/app.js
+++ js/app.js
@@ -58,6 +58,12 @@ class CalorieTracker {
   this._render();
  }
 
+ setLimit(calorieLimit) {
+  this._calorieLimit = calorieLimit;
+  this._displayCaloriesLimit();
+  this._render();
+ }
+
  // Private Methods //
 
  _displayCaloriesTotal() {
@@ -220,6 +226,10 @@ class App {
   document
    .getElementById('reset')
    .addEventListener('click', this._reset.bind(this));
+
+  document
+   .getElementById('limit-form')
+   .addEventListener('submit', this._setLimit.bind(this));
  }
 
  _newItem(type, e) {
@@ -286,6 +296,24 @@ class App {
   document.getElementById('workout-items').innerHTML = '';
   document.getElementById('filter-meals').value = '';
   document.getElementById('filter-meals').value = '';
+ }
+
+ _setLimit(e) {
+  e.preventDefault();
+
+  const limit = document.getElementById('limit');
+
+  if (limit.value === '') {
+   alert('Please add a limit');
+   return;
+  }
+
+  this._tracker.setLimit(+limit.value);
+  limit.value = '';
+
+  const modalEl = document.getElementById('limit-modal');
+  const modal = bootstrap.Modal.getInstance(modalEl);
+  modal.hide();
  }
 }
 
