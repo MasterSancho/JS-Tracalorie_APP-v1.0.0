98f6e04574aa167d76e6aedf581ea2ff4c8b58cf
maltsevalex <57605252+MasterSancho@users.noreply.github.com>
2023-07-11 09:49:42 +0300

    016-->Updated

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ class CalorieTracker {
 
  removeMeal(id) {
   const index = this._meals.findIndex((meal) => meal.id === id);
+
   if (index !== -1) {
    const meal = this._meals[index];
    this._totalCalories -= meal.calories;
@@ -49,12 +50,13 @@ class CalorieTracker {
 
  removeWorkout(id) {
   const index = this._workouts.findIndex((workout) => workout.id === id);
+
   if (index !== -1) {
    const workout = this._workouts[index];
    this._totalCalories += workout.calories;
    Storage.updateTotalCalories(this._totalCalories);
    this._workouts.splice(index, 1);
-   Storage.removeMeal(id);
+   Storage.removeWorkout(id);
    this._render();
   }
  }
@@ -87,33 +89,40 @@ class CalorieTracker {
  }
 
  _displayCaloriesLimit() {
-  const caloriesLimitEl = document.getElementById('calories-limit');
-  caloriesLimitEl.innerHTML = this._calorieLimit;
+  const calorieLimitEl = document.getElementById('calories-limit');
+  calorieLimitEl.innerHTML = this._calorieLimit;
  }
 
  _displayCaloriesConsumed() {
   const caloriesConsumedEl = document.getElementById('calories-consumed');
+
   const consumed = this._meals.reduce(
    (total, meal) => total + meal.calories,
    0
   );
+
   caloriesConsumedEl.innerHTML = consumed;
  }
 
  _displayCaloriesBurned() {
   const caloriesBurnedEl = document.getElementById('calories-burned');
+
   const burned = this._workouts.reduce(
    (total, workout) => total + workout.calories,
    0
   );
+
   caloriesBurnedEl.innerHTML = burned;
  }
 
  _displayCaloriesRemaining() {
   const caloriesRemainingEl = document.getElementById('calories-remaining');
   const progressEl = document.getElementById('calorie-progress');
+
   const remaining = this._calorieLimit - this._totalCalories;
+
   caloriesRemainingEl.innerHTML = remaining;
+
   if (remaining <= 0) {
    caloriesRemainingEl.parentElement.parentElement.classList.remove('bg-light');
    caloriesRemainingEl.parentElement.parentElement.classList.add('bg-danger');
@@ -142,20 +151,20 @@ class CalorieTracker {
   mealEl.classList.add('card', 'my-2');
   mealEl.setAttribute('data-id', meal.id);
   mealEl.innerHTML = `
-  <div class="card-body">
-  <div class="d-flex align-items-center justify-content-between">
-    <h4 class="mx-1">${meal.name}</h4>
-    <div
-      class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
-    >
-      ${meal.calories}
+    <div class="card-body">
+    <div class="d-flex align-items-center justify-content-between">
+      <h4 class="mx-1">${meal.name}</h4>
+      <div
+        class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+      >
+        ${meal.calories}
+      </div>
+      <button class="delete btn btn-danger btn-sm mx-2">
+        <i class="fa-solid fa-xmark"></i>
+      </button>
     </div>
-    <button class="delete btn btn-danger btn-sm mx-2">
-      <i class="fa-solid fa-xmark"></i>
-    </button>
   </div>
-</div>
-  `;
+    `;
 
   mealsEl.appendChild(mealEl);
  }
@@ -166,20 +175,21 @@ class CalorieTracker {
   workoutEl.classList.add('card', 'my-2');
   workoutEl.setAttribute('data-id', workout.id);
   workoutEl.innerHTML = `
-  <div class="card-body">
-  <div class="d-flex align-items-center justify-content-between">
-    <h4 class="mx-1">${workout.name}</h4>
-    <div
-      class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
-    >
-      ${workout.calories}
+    <div class="card-body">
+    <div class="d-flex align-items-center justify-content-between">
+      <h4 class="mx-1">${workout.name}</h4>
+      <div
+        class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
+      >
+        ${workout.calories}
+      </div>
+      <button class="delete btn btn-danger btn-sm mx-2">
+        <i class="fa-solid fa-xmark"></i>
+      </button>
     </div>
-    <button class="delete btn btn-danger btn-sm mx-2">
-      <i class="fa-solid fa-xmark"></i>
-    </button>
   </div>
-</div>
-  `;
+    `;
+
   workoutsEl.appendChild(workoutEl);
  }
 
@@ -353,6 +363,7 @@ class App {
    alert('Please fill in all fields');
    return;
   }
+
   if (type === 'meal') {
    const meal = new Meal(name.value, +calories.value);
    this._tracker.addMeal(meal);
